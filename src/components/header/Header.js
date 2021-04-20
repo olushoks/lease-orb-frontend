4135d8da93d43be2d81783a2fb15e468735a17a2
Olukayode <olukayodeshokunbi@gmail.com>
2021-04-20 08:12:03 -0400

    Added basic initial styling to the Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,21 @@
+import "./Header.css";
+
 const Header = () => {
   return (
-    <header>
-      <p>Lease-Orb</p>
-      <nav>
-        <ul>
+    <>
+      <header className="header">
+        Lease-Orb
+        {/* <p className="header-logo">Lease-Orb</p> */}
+      </header>
+      <nav className="nav-section">
+        <ul className="nav-items">
           <li>Listed Lease</li>
           <li>Interested In</li>
           <li>Messages</li>
           <li>LogOut</li>
         </ul>
       </nav>
-    </header>
+    </>
   );
 };
 
