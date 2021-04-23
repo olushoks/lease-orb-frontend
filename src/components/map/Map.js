8db9305e5ec90e43056c421da4ebe7ae1ebe1c77
Olukayode <olukayodeshokunbi@gmail.com>
2021-04-22 20:29:16 -0400

    Created function to track the current latitude and longitude when user drags marker on map to a specific location

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,4 +1,6 @@
+import React, { Component, useState } from "react";
 import {
+  InfoWindow,
   withScriptjs,
   withGoogleMap,
   GoogleMap,
@@ -9,10 +11,38 @@ import APIKEY from "../../key";
 const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${APIKEY}&v=3.exp&libraries=geometry,drawing,places`;
 
 const Map = () => {
+  // STATE VARIABLES
+  const [location, setLocation] = useState({
+    address: "",
+    city: "",
+    area: "",
+    state: "",
+  });
+  const [zoom, setZoom] = useState({ zoom: 15, height: 400 });
+  const [maxPosition, setMaxPosiition] = useState({ lat: 0, lng: 0 });
+  const [markerPosition, setMarkerPosition] = useState({ lat: 0, lng: 0 });
+
+  // GET LATITUDE AND LONGIITUDE
+  const onEndMarkerDrag = (event) => {
+    let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
+
+    console.log(newLat, newLng);
+  };
+
+  // MAP WRAPPER
   const MapWithAMarker = withScriptjs(
     withGoogleMap((props) => (
       <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+        <Marker
+          draggable={true}
+          onDragEnd={onEndMarkerDrag}
+          position={{ lat: -34.397, lng: 150.644 }}
+        >
+          <InfoWindow>
+            <div>My Map</div>
+          </InfoWindow>
+        </Marker>
       </GoogleMap>
     ))
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// ReactDOM.render(
-//   <React>
-//     <App />
-//   </React>,
-//   document.getElementById("root")
-// );
 ReactDOM.render(
   <React.StrictMode>
     <App />
