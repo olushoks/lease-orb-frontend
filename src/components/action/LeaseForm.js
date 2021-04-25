bda7b85e9d41269247c583ad2f443884a85df3fb
Olukayode <olukayodeshokunbi@gmail.com>
2021-04-25 13:01:41 -0400

    Created handleClick function to toggle LeaseForm on/off

--- src/components/action/LeaseForm.js
+++ src/components/action/LeaseForm.js
@@ -1,31 +1,53 @@
-const LeaseForm = () => {
+const LeaseForm = ({ closeForm }) => {
   return (
     <>
+      <span onClick={closeForm}>
+        <i className="fas fa-window-close"></i>
+      </span>
       <div>
         <p>Enter Lease Details as accurately as possible</p>
         <p>Success or Error Messages goes here</p>
       </div>
       <form>
-        <label id="property-name" htmlFor="property-name">
-          Apartment Name
-        </label>
-        <input type="text" name="property-name" value="QVC HOMES"></input>
-        <label id="address" htmlFor="address">
-          Address
-        </label>
-        <input type="text" name="address" value="QVC HOMES"></input>
-        <label id="monthly-rent" htmlFor="monthly-rent">
-          Rent Per Month
-        </label>
-        <input type="number" name="rent"></input>
-        <label id="available-date" htmlFor="available-date">
-          Available as of?
-        </label>
-        <input type="date" name="property-name"></input>
-        <label id="additional-info" htmlFor="additional-info">
-          Additional Information
-        </label>
-        <input type="textarea" name="additional-info" value="QVC HOMES"></input>
+        <label htmlFor="property-name">Apartment Name</label>
+        <input
+          type="text"
+          id="property-name"
+          name="property-name"
+          value="QVC HOMES"
+        ></input>
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
+          value="QVC HOMES"
+        ></input>
+        <label htmlFor="monthly-rent">Rent Per Month</label>
+        <input
+          type="number"
+          min="0"
+          step="50"
+          id="monthly-rent"
+          name="rent"
+        ></input>
+        <label htmlFor="available-date">Available as of?</label>
+        <input
+          type="date"
+          id="available-date"
+          name="property-name"
+          min="2021-04-25"
+          max="2021-08-31"
+          //   value="2021-04-25"
+        ></input>
+        <label htmlFor="additional-info">Additional Information</label>
+        <input
+          type="textarea"
+          id="additional-info"
+          name="additional-info"
+          value="QVC HOMES"
+        ></input>
+        <button className="btn">Submit</button>
       </form>
     </>
   );
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,42 @@
 import "./Main.css";
 //import Map from "../map/Map";
 import LeaseForm from "../action/LeaseForm";
+import { useState } from "react";
 
 const Main = ({ user }) => {
+  const [showActionBtn, setShowActionBtn] = useState(true);
+  const [action, setAction] = useState(null);
+
+  const handleClick = (action) => {
+    switch (action) {
+      case "listALease":
+        setAction(true);
+        setShowActionBtn(false);
+        break;
+      case "searchLease":
+        setAction(true);
+        setShowActionBtn(false);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="main">
       <p>Welcome {user.username}, what would you like to do today?</p>
-      <button className="btn">List A Lease</button>
-      <button className="btn">Search Available Leases</button>
+      {showActionBtn && (
+        <>
+          <button className="btn" onClick={() => handleClick("listALease")}>
+            List A Lease
+          </button>
+          <button className="btn" onClick={() => handleClick("searchLease")}>
+            Search Available Leases
+          </button>
+        </>
+      )}
       {/* <Map /> */}
-      <LeaseForm />
+      {action && <LeaseForm />}
     </div>
   );
 };
