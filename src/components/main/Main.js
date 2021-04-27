6933750295e6706b4a75e4be34c52d34ab952e9a
Olukayode <olukayodeshokunbi@gmail.com>
2021-04-26 20:39:33 -0400

    Created a checkForActiveLease function to restrict user from accessing List A Lease feature if they already have an actve lease

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,17 +6,30 @@ import { useState } from "react";
 const Main = ({ user }) => {
   const [showActionBtn, setShowActionBtn] = useState(true);
   const [action, setAction] = useState(null);
+  const [error, setError] = useState(null);
 
   const closeForm = () => {
     setShowActionBtn(true);
     setAction(null);
   };
 
+  const checkForActiveLease = () => {
+    if (user.listedLease.length !== 0) {
+      setError(
+        "You are not allowed to have more than one active leases! Go to your ListedLease to either edit or delete to continue"
+      );
+    } else {
+      setAction(true);
+      setShowActionBtn(false);
+    }
+  };
+
   const handleClick = (action) => {
     switch (action) {
       case "listALease":
-        setAction(true);
-        setShowActionBtn(false);
+        checkForActiveLease();
+        // setAction(true);
+        // setShowActionBtn(false);
         break;
       case "searchLease":
         setAction(true);
@@ -38,6 +51,7 @@ const Main = ({ user }) => {
           <button className="btn" onClick={() => handleClick("searchLease")}>
             Search Available Leases
           </button>
+          {<div>{error}</div>}
         </>
       )}
       {action && <ListLease closeForm={closeForm} />}
